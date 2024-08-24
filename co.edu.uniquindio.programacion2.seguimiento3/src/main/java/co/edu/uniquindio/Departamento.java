3ea27d33263fde42616d4486c68b536180134f54
jfsc27 <juanf.smithc@uqvirtual.edu.co>
2024-08-23 20:23:01 -0500

    Commit Empleado, Departamento
    Se añaden las clases Empleado y departamento

---
+++ co.edu.uniquindio.programacion2.seguimiento3/src/main/java/co/edu/uniquindio/Departamento.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio;
+
+public class Departamento {
+
+    private String nombre;
+    private String codigo;
+
+}
---
+++ co.edu.uniquindio.programacion2.seguimiento3/src/main/java/co/edu/uniquindio/Empleado.java
@@ -0,0 +1,38 @@
+package co.edu.uniquindio;
+
+public abstract class Empleado {
+
+    private String nombre;
+    private String idEmpleado;
+    private Departamento departamento;
+
+    public Empleado(String nombre, String idEmpleado, Departamento departamento) {
+        this.nombre = nombre;
+        this.idEmpleado = idEmpleado;
+        this.departamento = departamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
