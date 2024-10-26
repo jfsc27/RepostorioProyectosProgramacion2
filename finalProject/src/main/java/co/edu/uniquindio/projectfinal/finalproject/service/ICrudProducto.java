4773cd38093e8bbe9251c9dd59ed6b68fb8c63bb
Juan27g <159400013+Juan27g@users.noreply.github.com>
2024-10-26 17:22:01 -0500

    corrección

--- finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/service/ICrudProducto.java
+++ finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/service/ICrudProducto.java
@@ -1,4 +1,10 @@
 package co.edu.uniquindio.projectfinal.finalproject.service;
 
+import co.edu.uniquindio.projectfinal.finalproject.model.Producto;
+
 public interface ICrudProducto {
+    boolean crearProducto(Producto producto);
+    boolean eliminarProducto(Producto producto);
+    boolean actualizarProducto(Producto producto);
+
 }
--- finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/service/IModelFactoryServices.java
+++ finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/service/IModelFactoryServices.java
@@ -1,18 +1,22 @@
 package co.edu.uniquindio.projectfinal.finalproject.service;
 
+import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.ProductoDTO;
 import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.PublicacionDTO;
 import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.UsuarioDTO;
+import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.VendedorDTO;
 import co.edu.uniquindio.projectfinal.finalproject.model.Muro;
 import co.edu.uniquindio.projectfinal.finalproject.model.Usuario;
+
+import java.util.List;
 
 public interface IModelFactoryServices {
     public UsuarioDTO getUsuario(UsuarioDTO usuario);
     public boolean validarLogin(UsuarioDTO usuario);
     public Usuario getUsuarioCompleto(UsuarioDTO usuario);
     public void darMeGustaPublicacion(UsuarioDTO usuario, PublicacionDTO publicacion);
-    public List<ProductoDto> getListaProductosDisponibles(UsuarioDTO usuario);
-    boolean agregarPublicacion(PublicacionDTO publicacion, VendedorDto vendedor);
-    boolean eliminarPublicacion(PublicacionDTO publicacion, VendedorDto vendedor);
-    boolean actualizarPublicacion(PublicacionDTO publicacion, VendedorDto vendedor);
-    List<PublicacionDto> getListaPublicaciones(Muro muro);
+    public List<ProductoDTO> getListaProductosDisponibles(UsuarioDTO usuario);
+    boolean agregarPublicacion(PublicacionDTO publicacion, VendedorDTO vendedor);
+    boolean eliminarPublicacion(PublicacionDTO publicacion, VendedorDTO vendedor);
+    boolean actualizarPublicacion(PublicacionDTO publicacion, VendedorDTO vendedor);
+    List<PublicacionDTO> getListaPublicaciones(Muro muro);
 }
