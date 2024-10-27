01ac1260679324cac53fc3f156cdec3d35685015
Juan27g <159400013+Juan27g@users.noreply.github.com>
2024-10-26 23:47:41 -0500

    Muro y estadisticas

--- finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/viewController/MuroViewController.java
+++ finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/viewController/MuroViewController.java
@@ -1,4 +1,58 @@
 package co.edu.uniquindio.projectfinal.finalproject.viewController;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+
 public class MuroViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> cb1Vend;
+
+    @FXML
+    private ComboBox<?> cb2Vend;
+
+    @FXML
+    private ComboBox<?> cbProd;
+
+    @FXML
+    private ImageView marketplacepng;
+
+    @FXML
+    private Label productos;
+
+    @FXML
+    private Button reportebttn;
+
+    @FXML
+    private ScrollPane scrollEstProd;
+
+    @FXML
+    private ScrollPane scrollEstVend;
+
+    @FXML
+    private ScrollPane scrollUltProduc;
+
+    @FXML
+    private ScrollPane scrollcontactos;
+
+    @FXML
+    private ScrollPane scrollprod;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
