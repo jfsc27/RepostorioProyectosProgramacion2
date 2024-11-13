0be3f59c7a810e77fe7f7c3f8afd62a2b5869e32
jfsc27 <juanf.smithc@uqvirtual.edu.co>
2024-11-13 09:39:49 -0500

    Commit

--- finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/factory/ModelFactory.java
+++ finalProject/src/main/java/co/edu/uniquindio/projectfinal/finalproject/factory/ModelFactory.java
@@ -1,19 +1,28 @@
 package co.edu.uniquindio.projectfinal.finalproject.factory;
 
+import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.ProductoDTO;
+import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.PublicacionDTO;
+import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.UsuarioDTO;
+import co.edu.uniquindio.projectfinal.finalproject.mapping.dto.VendedorDTO;
 import co.edu.uniquindio.projectfinal.finalproject.mapping.mappers.MarketPlaceMappingImpl;
-import co.edu.uniquindio.projectfinal.finalproject.model.MarketPlace;
-import co.edu.uniquindio.projectfinal.finalproject.model.Vendedor;
+import co.edu.uniquindio.projectfinal.finalproject.model.*;
+import co.edu.uniquindio.projectfinal.finalproject.service.IModelFactoryServices;
 import org.controlsfx.control.PropertySheet;
 
-public class ModelFactory {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
+public class ModelFactory implements IModelFactoryServices {
     private static ModelFactory instance;
+    private static MarketPlace marketPlace;
     private static MarketPlaceMappingImpl mapping;
 
     private ModelFactory() {
         inicializarDatos();
         mapping = new MarketPlaceMappingImpl();
-
+        
     }
     public static ModelFactory getInstance() {
         if (instance == null) {
@@ -21,21 +30,130 @@ public class ModelFactory {
         }
         return instance;
     }
-    private void inicializarDatos() {
-        MarketPlace m = new MarketPlace();
-        Vendedor v = new Vendedor("costeña", "sapaperra", "1234", "000", "user", "123", "01");
+
+    @Override
+    public UsuarioDTO getUsuario(UsuarioDTO usuario) {
+        return null;
+    }
+
+    @Override
+    public boolean validarLogin(UsuarioDTO usuario) {
+        return false;
+    }
+
+    @Override
+    public Usuario getUsuarioCompleto(UsuarioDTO usuario) {
+        return null;
+    }
+
+    @Override
+    public void darMeGustaPublicacion(UsuarioDTO usuario, PublicacionDTO publicacion) {
 
     }
 
-    public static void setInstance(ModelFactory instance) {
-        ModelFactory.instance = instance;
+    @Override
+    public List<ProductoDTO> getListaProductosDisponibles(UsuarioDTO usuario) {
+        return List.of();
     }
 
-    public static MarketPlaceMappingImpl getMapping() {
-        return mapping;
+    @Override
+    public boolean agregarPublicacion(PublicacionDTO publicacion, VendedorDTO vendedor) {
+        return false;
     }
 
-    public static void setMapping(MarketPlaceMappingImpl mapping) {
-        ModelFactory.mapping = mapping;
+    @Override
+    public boolean eliminarPublicacion(PublicacionDTO publicacion, VendedorDTO vendedor) {
+        return false;
+    }
+
+    @Override
+    public boolean actualizarPublicacion(PublicacionDTO publicacion, VendedorDTO vendedor) {
+        return false;
+    }
+
+    @Override
+    public List<PublicacionDTO> getListaPublicaciones(Muro muro) {
+        return List.of();
+    }
+
+    private static void inicializarDatos() {
+
+        MarketPlace marketPlace1 = new MarketPlace();
+
+        //Creacion de productos
+        Producto producto1 = new Producto("Mazda carro", "/co/edu/uniquindio/marketpruebas/imagenes/Mazda.jpeg", "Vehiculos usados", Estado.PUBLICADO, 40000000);
+        Producto producto2 = new Producto("Nintendo Switch", "/co/edu/uniquindio/marketpruebas/imagenes/Nintendo-Switch.jpg","Consolas de video",Estado.PUBLICADO, 500000);
+        Producto producto3 = new Producto("Closet de dos puertas", "/co/edu/uniquindio/marketpruebas/imagenes/Closet-dos.png","Muebles para el hogar",Estado.PUBLICADO, 450000);
+        Producto producto4 = new Producto("Iphone 25", "/co/edu/uniquindio/marketpruebas/imagenes/iphone 25.jpeg","Celulares",Estado.PUBLICADO, 450000);
+        Producto producto5 = new Producto("Moto cualquiera","/co/edu/uniquindio/marketpruebas/imagenes/Moto.png", "Vehiculos",null,2500000);
+
+        //Creacion de publicaciones
+        Publicacion publicacion = new Publicacion(LocalDate.now(), LocalTime.now(), producto1,"Flamante vehiculo mazda dos dias de uso, mas informacion al interno");
+        Publicacion publicacion2 = new Publicacion(LocalDate.now().plusDays(1),LocalTime.now(),producto2,"Espectacular consola de video, nintendo porfavor no me demandes, mas informacion al interno");
+        Publicacion publicacion3 = new Publicacion(LocalDate.now().plusDays(2),LocalTime.now(),producto3,"Closet de dos puertas, de 3 metros de alto y 1,5 de ancho, mdf, mas info al interno");
+        Publicacion publicacion4 = new Publicacion(LocalDate.now().plusDays(3),LocalTime.now(),producto4,"Unico en el mundo, iphone 25, extraido de los laboratorios de apple, promosion del 10% al primer interesado, precio real: 1 billon de dolars");
+
+        //Creacion de vendedores
+        Vendedor vendedor1 = new Vendedor("Pepe", "Bermuda", "00001", "Alli al lado", "pepito07", "1234", "01");
+        Vendedor vendedor2 = new Vendedor("Gustava", "Santos", "00002", "Alli al otro lado", "Pollos08", "12345", "02");
+        Vendedor vendedor3 = new Vendedor("Martin", "Santos", "00003", "Alli", "user01", "00001", "03");
+        Vendedor vendedor4 = new Vendedor("Pedro", "Martinez", "00004", "Alli", "user02", "00002", "04");
+        Vendedor vendedor5 = new Vendedor("vendedor5","apellido5","00005","Alli","user05","00005","05");
+        Vendedor vendedor6 = new Vendedor("vendedor6","apellido6","00006","Alli","user06","00006","06");
+        Vendedor vendedor7 = new Vendedor("vendedor7","apellido7","00007","Alli","user07","00007","07");
+        Vendedor vendedor8 = new Vendedor("vendedor8", "apellido8","00008","Alli","user08","00008","08");
+        Vendedor vendedor9 = new Vendedor("vendedor9","apellido9","00009","Alli","user09","00009","09");
+        Vendedor vendedor10 = new Vendedor("vendedor10", "apellido10","000010","Alli","user10","000010","10");
+        Vendedor vendedor11 = new Vendedor("vendedor11","apellido11","000011","Alli","user11","000011","11");
+
+        //Creacion de administradores
+        Administrador admin = new Administrador("Admin", "admin", "00000", "desconocida", "admin", "admin","00");
+
+        //Creacion de comentarios
+        Comentario comentario = new Comentario(vendedor4, LocalDate.now(), LocalTime.now(),"Carisimo");
+        Comentario comentario2 = new Comentario(vendedor2,LocalDate.now().plusDays(2),LocalTime.now(),"Esta feo");
+
+        //Agregar comentario a publicaciones
+        publicacion4.agregarComentario(comentario);
+        publicacion.agregarComentario(comentario2);
+
+        //Creacion de muros
+        Muro muro = new Muro();
+        Muro muro2 = new Muro();
+
+        //Agregar publicaciones al muro
+        muro.agregarPublicacion(publicacion);
+        muro.agregarPublicacion(publicacion2);
+        muro.agregarPublicacion(publicacion3);
+        muro2.agregarPublicacion(publicacion4);
+
+        //Agregar muro a un vendedor
+        vendedor1.setMuro(muro);
+        vendedor2.setMuro(muro2);
+
+        //Agregar Producto a vendedor
+        vendedor1.agregarProducto(producto5);
+        vendedor1.agregarProducto(producto1);
+        vendedor1.agregarProducto(producto2);
+        vendedor1.agregarProducto(producto3);
+        vendedor2.agregarProducto(producto4);
+
+        //Relacionar contactos bilateralmente
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor2);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor3);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor4);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor5);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor6);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor7);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor8);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor9);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor10);
+        marketPlace1.agregarContactosEntreSi(vendedor1,vendedor11);
+
+        //Agregar distintos objetos al marketplace automaticamente
+        List<Object> parametros = Arrays.asList(vendedor1,vendedor2,admin);
+        parametros.forEach(marketPlace1::agregarAutomatico);
+
+        marketPlace = marketPlace1;
     }
 }
